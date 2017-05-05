f114ee2185f39d8d7adf8f2cce7f066022fbd3fc
kohlbrr <kohlbrr@gmail.com>
2017-05-05 16:35:42 -0400

    On to part 3

--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
-//const Sequelize = require('sequelize');
 const db = require('./db');
 const Place = require('./place');
 const Hotel = require('./hotel');
 const Activity = require('./activity');
 const Restaurant = require('./restaurant');
 
-/*
-// Do model stuff here
-const db = new Sequelize('postgres://localhost:5432/trip_planner', {
-  logging: false
-});
-*/
-
 Hotel.belongsTo(Place);
 Activity.belongsTo(Place);
 Restaurant.belongsTo(Place);
+
+//module.exports = db;
+module.exports = {
+  Place: Place,
+  Hotel: Hotel,
+  Activity: Activity,
+  Restaurant: Restaurant
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
-//const models = require('../models');
+const models = require('../models');
+const Hotel = models.Hotel;
+const Restaurant = models.Restaurant;
+const Activity = models.Activity;
 
 router.get('/', (req, res, next) => {
-  res.render('../index');
+  let renderValues = {};
+  Hotel.findAll({})
+  .then((hotels) => {
+    renderValues.hotels = hotels;
+    return Restaurant.findAll({});
+  })
+  .then((restaurants) => {
+    renderValues.restaurants = restaurants;
+    return Activity.findAll({});
+  })
+  .then((activities) => {
+    renderValues.activities = activities;
+    res.render('index', renderValues);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 })
 
 module.exports = router;
